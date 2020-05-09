0848f803889628dc03e1a60f1df85b40fb7a0475
Jiangqiu Shen <jshen2@mtu.edu>
2020-05-08 22:45:45 -0400

    fix bug

---
+++ after_chroot.sh
@@ -0,0 +1,18 @@
+ls -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+hwclock --systohc
+sed 's/#en_US.U/en_US.U/' /etc/locale.gen
+locale-gen
+echo LANG=en_US.UTF-8 > locale.conf
+systemctl enable dhcpcd
+systemctl start dhcpcd
+pacman -Syy
+pacman -S grub efibootmgr
+pacman -S vim amd-ucode
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+pacman -S yay
+yay -Fyy
+yay -S deepin  networkmanager  lightdm
+systemctl enable lightdm 
+systemctl enable NetworkManager
+yay -S nginx aria2c-daemon webui-aria2 visual-studio-code-bin
