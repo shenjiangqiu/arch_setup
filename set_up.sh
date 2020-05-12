5e354bda959e2ff05e51eadcb71fce8e08a011a5
shenjiangqiu <ivy22233qiu@live.com>
2020-05-12 15:06:39 -0400

    Update set_up.sh

--- set_up.sh
+++ set_up.sh
@@ -18,8 +18,9 @@ echo '[archlinuxcn]
 Server = https://mirror.xtom.com/archlinuxcn/$arch' >> /etc/pacman.conf
 echo "installing package to /mnt"
 pacman -Syy
-pacstrap /mnt base base-devel linux linux-firmware dhcpcd  wireless_tools iw wap_supplicant iwd netctl dialog ppp
+pacstrap /mnt base  linux linux-firmware 
 genfstab -U /mnt >> /mnt/etc/fstab
 cp  /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+cp /etc/pacman.conf /mnt/etc/pacman.conf
 echo "try arch-chroot /mnt"
 
