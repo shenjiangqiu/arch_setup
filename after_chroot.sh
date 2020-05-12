7b13af44dac3a7fe59bb19f6e798709b24f92378
shenjiangqiu <ivy22233qiu@live.com>
2020-05-12 15:08:46 -0400

    Update after_chroot.sh

--- after_chroot.sh
+++ after_chroot.sh
@@ -1,21 +1,18 @@
-echo '[archlinuxcn]
-Server = https://mirror.xtom.com/archlinuxcn/$arch' >> /etc/pacman.conf
 ls -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 hwclock --systohc
 sed 's/#en_US.U/en_US.U/' /etc/locale.gen
 locale-gen
-echo LANG=en_US.UTF-8 > /etc/locale.conf
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 systemctl enable dhcpcd
 systemctl start dhcpcd
+read a
+ping google.com
 pacman -Syy
 pacman -S archlinuxcn-keyring
 pacman -S grub efibootmgr
-pacman -S vim amd-ucode
+pacman -S vim amd-ucode 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 pacman -S yay
 pacman -Fyy
-pacman -S deepin  networkmanager  lightdm
-systemctl enable lightdm 
-systemctl enable NetworkManager
-pacman -S nginx aria2c-daemon webui-aria2 visual-studio-code-bin
+pacman -S iw
