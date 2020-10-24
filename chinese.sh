226df4f56b70843b1c6f5c6ffa321111c9b5b33e
Jiangqiu Shen <ivy22233qiu@live.com>
2020-10-24 04:56:21 -0400

    Create chinese.sh

---
+++ chinese.sh
@@ -0,0 +1,1 @@
+yay -S adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts wqy-microhei wqy-zenhei wqy-bitmapfont  	ibus-table  ibus-chewing  ibus-googlepinyin  	ibus-libpinyin ibus-sunpinyin  	ibus-rime
